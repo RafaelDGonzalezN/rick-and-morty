35dbd4f579919449940ce04fd398858d2f6b1928
RafaelDGonzalezN <david.gnzlz.224@gmail.com>
2023-11-18 07:41:36 -0500

    Actualizacion M4

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,15 +1,26 @@
-const users = require ("../utils/users")
+const {User} = require("../DB_connection")
 
-const login = (req, res) =>{
-    const {email, password} = req.query
-    let access = false
+const login = async (req, res) => {
+    let {email, password} = req.query
 
-    users.forEach(user => {
-        if(user.email === email && user.password === password){
-            access = true
+    try {
+        if(email && password){
+            const foundUser = await User.findOne({
+                where: {email}
+            })
+            if(!foundUser) {
+                return res.status(404).json({message: "Usuario no encontrado"})
+            }
+            if(foundUser.password !== password){
+                return res.status(403).json({message: "Contraseña incorrecta"})
+            }
+            return res.status(200).json({access: true})
         }
-        return res.status(200).json({access})        
-    });
+        return res.status(400).json({message: "Faltan datos"})
+    } catch (error) {
+        return res.status(500).json({message: error})
+    }
+
 }
 
 module.exports = login;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,56 +1,9 @@
-// const http = require("http");
-// const characters = require("./utils/data")
-// const getCharByID = require("./controllers/getCharById")
-
-// http.createServer((req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    
-//     const { url } = req;
-
-
-//     // if(url.includes("/rickandmorty/character")){
-//     //     const id = Number(url.split("/").pop())
-//     //     const foundCh = characters.find((ch) => ch.id === id)
-
-//     if(url.includes("/rickandmorty/character")){
-//         const id = Number(url.split("/").pop())
-//         getCharByID(res,id);
-
-//         // if(foundCh){
-//         //     res.writeHead(200,{"Content-Type": "application/json"})
-//         //     return res.end(JSON.stringify(foundCh)) 
-//         // }else{
-//         //     res.writeHead(404,{"Content-Type": "application/json"})
-//         //     return res.end(JSON.stringify({error: "Character not found"})) 
-//         // }
-
-//     }
-// })
-// .listen(3001, "localhost")
-
-// const express = require('express');
-// const router = require ("./routes/index")
-// const server = express();
 const server = require("./app")
+const { conn } = require('./DB_connection');
 const PORT = 3001;
 
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header(
-//        'Access-Control-Allow-Headers',
-//        'Origin, X-Requested-With, Content-Type, Accept'
-//     );
-//     res.header(
-//        'Access-Control-Allow-Methods',
-//        'GET, POST, OPTIONS, PUT, DELETE'
-//     );
-//     next();
-//  });
-//  server.use(express.json())
-//  server.use("/rickandmorty", router)
-
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+conn.sync({force: false}).then(() => {
+   server.listen(PORT, () => {
+      console.log('Server raised in port: ' + PORT);
+   });
+})
